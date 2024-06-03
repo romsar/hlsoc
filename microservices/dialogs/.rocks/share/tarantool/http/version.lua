2d5d3147a003c729ed11a92244a822e322826acd
Roman Sarvarov <r.sarvarov@vk.team>
2024-06-04 01:09:48 +0300

    clear git cache

--- microservices/dialogs/.rocks/share/tarantool/http/version.lua
+++
@@ -1,4 +0,0 @@
--- Сontains the module version.
--- Requires manual update in case of release commit.
-
-return '1.5.0'
--- microservices/dialogs/.rocks/share/tarantool/vshard_0_1_25_1-vshard/init.lua
+++
@@ -1,9 +0,0 @@
-local consts = require('vshard.consts')
-
-return {
-    _VERSION = consts.VERSION,
-    router = require('vshard.router'),
-    storage = require('vshard.storage'),
-    consts = consts,
-    error = require('vshard.error'),
-}
